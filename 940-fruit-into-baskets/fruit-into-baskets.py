668f78b98a3f32e8ea1dab391b29693068af69c2
Kuljot Kaur <kaurkuljot23@gmail.com>
2026-09-09 22:31:34 +0530

    Time: 169 ms (83.37%) | Memory: 26 MB (34.91%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        seen = {}
+        left = 0
+        maxf = 0
+        for right in range(len(fruits)):
+            seen[fruits[right]] = seen.get(fruits[right], 0) + 1
+            while len(seen) > 2:
+                seen[fruits[left]] -= 1
+                if seen[fruits[left]] == 0:
+                    del seen[fruits[left]]
+                left += 1
+            cw = right - left + 1
+            maxf = max(maxf, cw)
+        return maxf    
+
+        return maxf
